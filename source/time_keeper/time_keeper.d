05aea7a13429630fc7c2f9629baf9e028d2f70e5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-06 19:45:31 -0500

    Modify time keeper to be simpler

--- source/time_keeper/time_keeper.d
+++ source/time_keeper/time_keeper.d
@@ -2,11 +2,7 @@ module time_keeper.time_keeper;
 
 import core.time;
 
-import game;
-
 public class TimeKeeper {
-
-    private Game game;
 
     // Start with delta of a HUGE amount, limited by maxDelta
     private MonoTime before;
@@ -15,22 +11,18 @@ public class TimeKeeper {
     // High precision delta for users with powerful computers
     private double delta = 0;
 
-    this(Game game){
-        this.game = game;
+    this(){
         before = MonoTime.currTime();
         after = MonoTime.currTime();
     }
 
     // Calculates the delta time between frames. First will always be 0
-    void calculateDelta() {
+    double calculateDelta() {
         after = MonoTime.currTime;
         Duration duration = after - before;
         delta = cast(double)duration.total!("nsecs") / 1_000_000_000.0;        
         before = MonoTime.currTime;
-    }
 
-    // Allows any function or object to get the delta anywhere in the program
-    double getDelta() {
         return delta;
-    }
+    }    
 }
