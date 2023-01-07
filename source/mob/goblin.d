2609637e1b3f3ffe7e0f8026a5e84cd632a568ae
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-06 21:49:23 -0500

    Create goblin.d

---
+++ source/mob/goblin.d
@@ -0,0 +1,22 @@
+module mob.goblin;
+
+import mob.mob;
+
+public class Goblin : MobInterface {
+    
+    public void onHurt() {
+
+    }
+
+    public void onMove() {
+
+    }
+
+    public void onSpawn() {
+
+    }
+
+    public void onTick() {
+        
+    }
+}
