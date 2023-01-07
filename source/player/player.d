17a9cb1924e75e4b3ee6e362b6cc4fa9985ac769
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-06 20:38:58 -0500

    Draw player & controls

--- source/player/player.d
+++ source/player/player.d
@@ -1,5 +1,6 @@
 module player.player;
 
+import raylib;
 import std.stdio;
 import position.vec2;
 
@@ -20,6 +21,40 @@ public class Player {
 
     public void moveY( int y ){
         position.addY( y );
+    }
+
+    public int getX() {
+        return position.getX();
+    }
+
+    public int getY() {
+        return position.getY();
+    }
+
+    public void doControls() {
+        if (IsKeyPressed(KeyboardKey.KEY_LEFT)) {
+            if ( this.getX() > 0 ) {
+                this.moveX( -1 );
+            }
+        } else if ( IsKeyPressed(KeyboardKey.KEY_RIGHT) ) {
+            if ( this.getX() < 9 ) {
+                this.moveX( 1 );
+            }
+        } else if ( IsKeyPressed(KeyboardKey.KEY_UP) ) {
+            if ( this.getY() > 0 ) {
+                this.moveY( -1 );
+            }
+        } else if ( IsKeyPressed(KeyboardKey.KEY_DOWN) ) {
+            if ( this.getY() < 9 ) { 
+                this.moveY( 1 );
+            }
+        }
+    }
+
+    public void draw() {
+        int fontSize = 60;
+
+        DrawText("X", this.getX() * fontSize, this.getY() * fontSize, fontSize, Colors.RED);
     }
 
     public void debugPrint() {
