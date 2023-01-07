63dcd9cbbbaaf33a785e891e8f2cdfb1870d75b2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-06 19:48:25 -0500

    Make a window open

--- source/app.d
+++ source/app.d
@@ -5,11 +5,18 @@ import player.player;
 
 void main() {
 
-    
+    validateRaylibBinding();
+    InitWindow( 800, 600, "minimal_rpg" );
+    SetTargetFPS(30);
 
     Player player = new Player( "debug player", 0, 0 );
 
     player.debugPrint();
 
+    while ( !WindowShouldClose() ) {
+        writeln("hi");
+    }
+
+    CloseWindow();
 }
 
